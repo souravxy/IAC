8cdb9414ad41b8a93294a62cf30cb17b777819b2
Ubuntu <ubuntu@ip-172-31-29-92.us-east-2.compute.internal>
2020-07-26 17:10:08 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the docker image
-sudo docker build -t cnid_infra .	
+sudo docker build -t cnid_infra_image .	
 
 # Remove swarm if already a part of
 sudo docker swarm leave --force	 2>&1 > /dev/null
@@ -13,8 +13,7 @@ sudo docker swarm init | grep -i "join --token" > swarm_token.sh
 # and they will become a part of the swarm as well, and container will run on them as well.
 
 # Remove any existing service for cnid_infra
-sudo docker service rm cnid_infra_service
+sudo docker service rm cnid_infra_service 2>&1 > /dev/null
 
 # Create service for cnid_infra
-sudo docker service create --name cnid_infra_Service -p 80:80 --replicas 3 cnid_infra
-
+sudo docker service create --name cnid_infra_service -p 80:80 --replicas 3 cnid_infra_image
